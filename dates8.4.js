18bbad1c35d9b98edb8925d3e13f6e4401c18307
Antoine.C <chaponantoinepro@gmail.com>
2025-03-25 11:17:22 +0100

    fin dates

--- dates8.4.js
+++ dates8.4.js
@@ -1,5 +1,42 @@
+let clock = () => {
 const date = new Date();
-const options = {
+const options = new Intl.DateTimeFormat ("en-US", 
+AM).formatToParts(date);
+
+
+options.forEach(element => {
+switch(element.type){
+  case "weekday":
+    document.getElementsByClassName("day-name")[0].textContent = element.value;
+    break;
+  case "day":
+      document.getElementsByClassName("day")[0].textContent = element.value;
+      break;
+  case "month":
+    document.getElementsByClassName("month")[0].textContent = element.value;
+    break;
+  case "year":
+    document.getElementsByClassName("year")[0].textContent = element.value;
+    break;    
+  case "hour":
+    document.getElementsByClassName("time")[0].textContent = element.value;
+    break;
+   case "minute":
+    document.getElementsByClassName("time")[0].textContent +=`:${element.value}:`;
+    break;
+  case "second":
+    document.getElementsByClassName("time")[0].textContent += element.value;
+    break;
+    
+    }}
+);}
+
+
+let change = () => {
+  (AM.hour12) = (!AM.hour12)
+}
+
+const AM =   {
   weekday: "short",
   year: "numeric",
   month: "short",
@@ -7,4 +44,10 @@ const options = {
   hour: "2-digit",
   minute: "2-digit",
   second: "2-digit",
-};
+  hour12: true,}
+
+document.getElementsByClassName('time-box')[0].addEventListener("click", change);
+
+
+clock();
+setInterval(clock, 1000);
